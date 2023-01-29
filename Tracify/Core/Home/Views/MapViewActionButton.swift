6fbfda83bea75393188b592d6cce36efb68afab0
Jenna Lee <jennaolee03@gmail.com>
2023-01-28 18:17:30 -0800

    Added MapViewAction Button n to HomeView

--- Tracify/Core/Home/Views/MapViewActionButton.swift
+++ Tracify/Core/Home/Views/MapViewActionButton.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct MapViewActionButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            
+        } label: {
+            Image(systemName: "line.3.horizontal")
+                .font(.title2)
+                .foregroundColor(.black)
+                .padding()
+                .background(.white)
+                .clipShape(Circle())
+                .shadow(color: .black, radius: 6)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
---
+++ Tracify/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,26 @@
+//
+//  LocationSearchView.swift
+//  Tracify
+//
+//  Created by Stephen Lee on 2023-01-28.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    var body: some View {
+        
+        VStack {
+            // header view
+            
+            
+            // list view
+        }
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
